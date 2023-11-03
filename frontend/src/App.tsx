8f8c8f0b859a41e0f3daf816a1149e416f999b3e
Dominic Bergmann <dominic.bergmann@gmail.com>
2023-11-03 16:30:46 +0100

    Add absolutely visually stunning frontend for getAllMovies and addMovie

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,8 +1,74 @@
+import axios from "axios";
+import {ChangeEvent, useEffect, useState} from "react";
+import {Movie} from "./lib/types.tsx";
+
+
 
 export default  function App ():JSX.Element {
+  const [movies, setMovies] = useState<Movie[]>([]);
+    const [userInputTitle, setUserInputTitle] = useState<string>('');
+    const [userInputAuthor, setUserInputAuthor] = useState<string>('');
 
-  return (
-    <>
-    </>
-  )
+    const changeInputTitle = (event: ChangeEvent<HTMLInputElement>) => {
+        setUserInputTitle(event.target.value);
+    };
+
+    const changeInputAuthor = (event: ChangeEvent<HTMLInputElement>) => {
+        setUserInputAuthor(event.target.value);
+    };
+
+    function getMovies(){
+    axios.get('/api/movies')
+        .then(response => {
+          setMovies(response.data);
+        })
+        .catch(error => {
+          console.log(error);
+        });
+  };
+
+  useEffect(() => {
+    getMovies();
+  }, []);
+
+
+    const addMovie = (event: ChangeEvent<HTMLFormElement>) => {
+        event.preventDefault();
+        axios.post('/api/movies', {
+            title: userInputTitle,
+            author: userInputAuthor
+        })
+            .then(response => {
+                setMovies([...movies, response.data]);
+                setUserInputTitle('');
+                setUserInputAuthor('')
+            })
+            .catch(error => {
+                console.log(error);
+            });
+    };
+
+
+
+    return (
+        <>
+
+            <form onSubmit={addMovie}>
+                <input type="text" placeholder="Title" value={userInputTitle} onChange={changeInputTitle} />
+                <input type="text" placeholder="Author" value={userInputAuthor} onChange={changeInputAuthor} />
+                <button type="submit">Add</button>
+            </form>
+            {!movies ? <strong>movies are loading</strong> : (
+
+                <ul>
+                    {movies.map(movie => (
+                        <li key={movie.id}>
+                            <h3>Title: {movie.title}</h3>
+                            <p>Author: {movie.author}</p>
+                        </li>
+                    ))}
+                </ul>
+            )}
+        </>
+    );
 }
